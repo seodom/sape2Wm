1a4aa662dd6c60d19dda055250056076aa875924
Odarchenko N.D <odarchenko.n.d@gmail.com>
2013-01-07 15:21:40 +0200

    in process

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Скрипт для вывода баланса sape.ru на webmoney при достижении необходимой суммы
+ *
+ * @author Odarchenko N.D. <odarchenko.n.d@gmail.com>
+ * @created 07.01.13 11:44
+ *
+ */
+// Запускать по cron, желательно с интервалом не чаще 2 раз в сутки
+
+
+// Минимальная сумма на счету, при которой выводить
+$minSum = 1100;
+// Сколько выводить
+$sum = 1000; //Нельзя выводить меньше 150 рублей
+
+// Данные для входа в аккаунт sape.ru
+$login = 'webmaster1000';
+$password = '34555553';
+
+// Название файла для cookies (доступен для записи)
+$cookieFile = 'secretFileCookie_09824.tmp';
+
+//////////////////////////////////////// Настройки закончены, дальше скрипт все сделает ////////////////////////////////
+
+header('Content-Type: text/html;charset=UTF-8');
+
+if (!extension_loaded("curl"))
+{ // ссылка ведет на запрос в google "как установить curl"
+    die('<a href="http://goo.gl/ncGxw">cURL</a> extension is not available');
+}
+/*
+// Чистим файл
+@file_put_contents($cookieFile, '');
+
+if (!file_exists($cookieFile))
+{
+    die('cookie file not exists! check your config.');
+}
+
+
+// Проверяем что его удалось перезаписать
+if (file_get_contents($cookieFile) != '')
+{ // ссылка ведет на запрос в google "как установить права доступа 777 на папку"
+    die('cookie file <a href="http://goo.gl/3yCCM">not writable</a>!');
+}
+*/
+require 'sapeCurl.php';
+$s = new sapeCurl($cookieFile);
+// Авторизуемся на сайте
+//$login = $s->login($login, $password);
+
+$money = $s->getBalance();
+
+if ($money === FALSE)
+{
+    die('Error with getBalance! Check your script code');
+}
+
+echo 'Money: ', $money, '<br>';
+
+if ($money >= $minSum)
+{
+    $s->makeRequest4Payment($sum);
+    echo $s->getLastError();
+}
+else
+    echo 'Nothing to do.';
+
+// Чистим файл с куками в целях безопасности
+//@file_put_contents($cookieFile, '');
---
+++ sapeCurl.php
@@ -0,0 +1,200 @@
+<?php
+/**
+ * Sape.ru cUrl interaction class
+ *
+ * @author Odarchenko N.D. <odarchenko.n.d@gmail.com>
+ * @created 07.01.13 11:53
+ */
+class sapeCurl
+{
+    protected $cookieFile = '';
+    protected $lastError = '';
+
+    /**
+     * @param $cookieFile
+     */
+    public function __construct($cookieFile)
+    {
+        $this->cookieFile = $cookieFile;
+    }
+
+    /**
+     * Get cookie and save into cookieFile
+     *
+     * @param string $username
+     * @param string $pass
+     *
+     * @return string
+     */
+    public function login($username, $pass)
+    {
+        return $this->myCurl(
+            'https://auth.sape.ru/login/',
+            array(
+                 'act'      => 'login',
+                 'r'        => 'https://www.sape.ru',
+                 'username' => $username,
+                 'password' => $pass
+            )
+        );
+
+    }
+
+    /**
+     * Get last script error
+     *
+     * @return string
+     */
+    public function getLastError()
+    {
+        if (!$this->lastError)
+        {
+            return '';
+        }
+
+        return 'Error: ' . htmlspecialchars($this->lastError);
+    }
+
+    /**
+     * make a request for payment
+     *
+     * @param float $amount
+     *
+     * @return bool
+     */
+    public function makeRequest4Payment($amount)
+    {
+        $cookie = file_get_contents($this->cookieFile);
+        if (!$cookie)
+        {
+            $this->lastError = 'You need login before "makeRequest4Payment"';
+            return FALSE; //you need login
+        }
+
+        //$regexp = '|JSESSIONID\s+([0-9a-z]+)|i';
+
+        $page = $this->myCurl('http://passport.sape.ru/withdraw/webmoney/'); //getting JSESSIONID
+       /* $cookie = file_get_contents($this->cookieFile);
+        if (!preg_match($regexp, $cookie, $sid))
+        {
+            $this->lastError = 'regexp in "makeRequest4Payment" does not work!';
+            return FALSE;
+        }
+        */
+        if(!preg_match('|method="post" action="\.\./\.\.(/withdraw/webmoney/[^"]+)|', $page, $pUrl))
+        {
+            $this->lastError = 'regexp2 in "makeRequest4Payment" does not work!';
+            return FALSE;
+        }
+
+        echo 'http://passport.sape.ru' .  $pUrl[1], '<br>';
+
+        var_dump($amount);
+
+        $answer = $this->myCurl(
+        //http://passport.sape.ru/withdraw/webmoney/?wicket:interface=:1:withdrawFormContainer:form::IFormSubmitListener::
+            'http://passport.sape.ru' .  $pUrl[1],
+            array(
+                 'id0_hf_0'     => '',
+                 'id1_hf_0'     => '',
+                 'id2_hf_0'     => '',
+                 'id3_hf_0'     => '',
+                 'id4_hf_0'     => '',
+                 'id5_hf_0'     => '',
+                 'id6_hf_0'     => '',
+                 'id7_hf_0'     => '',
+                 'id8_hf_0'     => '',
+                 'id9_hf_0'     => '',
+                 'amount'       => $amount,
+                 'submitButton' => 'Создать заявку'
+            ),
+            'http://passport.sape.ru/withdraw/webmoney/'
+
+        );
+
+        if(preg_match('|(wicket[^\s]+)|', $answer, $p ))
+        {
+            echo 'http://passport.sape.ru/?' . $p[1], '<br>';
+            $answer = $this->myCurl(
+                'http://passport.sape.ru/?' . $p[1],
+                array(
+                     'idd_hf_0'     => '',
+                     'amount'       => $amount,
+                     'submitButton' => 'Создать заявку'
+                ));
+        }
+        print_r($answer);
+        return TRUE;
+    }
+
+    /**
+     * Get available money
+     *
+     * @return bool|float
+     */
+    public function getBalance()
+    {
+        $js = $this->myCurl(
+            'http://widget.sape.ru/balance/?alt=html&tpl=balance_main&container_id=balance_widget_src&charset=utf-8'
+        );
+
+        if (preg_match('|<th>Доступно</th>\s+<td colspan="2">\s*<b>([^<]+)</b>|', $js, $match))
+        {
+            //remove extra characters (ex. &nbsp;)
+            $amount = preg_replace("|[^0-9,]|", '', $match[1]);
+            return (float)str_replace(',', '.', $amount);
+        }
+
+        return FALSE;
+    }
+
+    /**
+     * Curl Request
+     *
+     * @param string $url
+     * @param string $post
+     * @param string $referer
+     *
+     * @return string
+     */
+    protected function myCurl($url, $post = '', $referer = '')
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        if (!$referer)
+        {
+            curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
+        }
+        else
+        {
+            curl_setopt($ch, CURLOPT_REFERER, $referer);
+        }
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20.0 Firefox/20.0');
+        curl_setopt($ch, CURLOPT_ENCODING, 'utf-8');
+
+        curl_setopt($ch, CURLOPT_TIMEOUT, 200);
+
+        if ($post)
+        {
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        }
+
+        if ($this->cookieFile)
+        {
+            curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookieFile);
+            curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookieFile);
+        }
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+//        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+}
